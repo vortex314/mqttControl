b13b64bbafa38988392c6e3494f6611b2c7aad46
Lieven Merckx <lieven.merckx@ing.com>
2021-04-24 01:39:40 +0200

    working on tree

--- src/components/MqttTree.vue
+++ src/components/MqttTree.vue
@@ -1,5 +1,19 @@
 <template>
-  <v-treeview class="tv" :items="items" dense></v-treeview>
+  <v-treeview
+    @update:active="clicked"
+    v-model="tree"
+    :items="items"
+    activatable
+    return-object="true"
+    dense
+    item-key="id"
+    ><template v-slot:label="{ item }" selectable="isLeaf(item)">
+      {{ item.name }}
+      <span v-if="isLeaf(item)" >
+        : {{ JSON.stringify(item.value) }}
+      </span>
+    </template>
+  </v-treeview>
 </template>
 
 <script>
@@ -10,133 +24,71 @@ export default {
   mixins: [mqttBase],
   data() {
     return {
-      items:[],
-      idCounter:0,
-    }
+      tree: [],
+      items: [],
+      idCounter: 0,
+    };
   },
   mounted() {
     this.mqttRegister(this, "src/#");
   },
   methods: {
-    onMqttMessage(topic,variant) {
-      this.updateTopic(topic,variant)
+    onMqttMessage(topic, variant) {
+      this.updateTopic(topic, variant);
     },
-    updateTopic(topic,variant) {
-      let parts=topic.split('/')
-      let childrenArray = this.items
-      let childObject=null
-      for( let part of parts ) {
-        childObject = this.hasChild(childrenArray,part)
-        if ( childObject == null ) {
-          console.log("add topic to tree",topic)
-          childObject = this.createParent(part)
-          childrenArray.push(childObject)
-          childrenArray.sort((a,b) => ( (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)))
+    updateTopic(topic, variant) {
+      let parts = topic.split("/");
+      let childrenArray = this.items;
+      let childObject = null;
+      for (let part of parts) {
+        childObject = this.hasChild(childrenArray, part);
+        if (childObject == null) {
+          console.log("add topic to tree", topic);
+          childObject = this.createParent(part);
+          childrenArray.push(childObject);
+          childrenArray.sort((a, b) =>
+            a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+          );
         } else {
-          childObject.count++
+          childObject.count++;
         }
-        childrenArray = childObject.children
+        childrenArray = childObject.children;
       }
-      childObject.value=variant
+      childObject.topic = topic
+      childObject.value = variant;
     },
-    hasChild(parent,x){
-      for( let i=0;i<parent.length;i++  ) {
-        if ( parent[i].name == x) return parent[i]
+    hasChild(parent, x) {
+      for (let i = 0; i < parent.length; i++) {
+        if (parent[i].name == x) return parent[i];
       }
-      return null
+      return null;
     },
     createParent(x) {
-      this.idCounter++
+      this.idCounter++;
       return {
-          id:this.idCounter,
-          name:x,
-          count:0,
-          value:null,
-          children:[]
-      }
+        id: this.idCounter,
+        name: x,
+        count: 0,
+        children: [],
+      };
     },
     createChild(x) {
-      this.idCounter++
+      this.idCounter++;
       return {
-          id:this.idCounter,
-          name:x,
-      }
-    }
+        id: this.idCounter,
+        name: x,
+      };
+    },
+    findItemById(id){
+      console.log(id)
+    },
+    isLeaf(obj){
+      return obj.value != undefined
+    },
+    clicked(ev) {
+      console.log(ev); // array of activated id's
+    },
   },
-  data2: () => ({
-    items: [
-      {
-        id: 1,
-        name: "Applications :",
-        children: [
-          { id: 2, name: "Calendar : app" },
-          { id: 3, name: "Chrome : app" },
-          { id: 4, name: "Webstorm : app" },
-        ],
-      },
-      {
-        id: 5,
-        name: "Documents :",
-        children: [
-          {
-            id: 6,
-            name: "vuetify :",
-            children: [
-              {
-                id: 7,
-                name: "src :",
-                children: [
-                  { id: 8, name: "index : ts" },
-                  { id: 9, name: "bootstrap : ts" },
-                ],
-              },
-            ],
-          },
-          {
-            id: 10,
-            name: "material2 :",
-            children: [
-              {
-                id: 11,
-                name: "src :",
-                children: [
-                  { id: 12, name: "v-btn : ts" },
-                  { id: 13, name: "v-card : ts" },
-                  { id: 14, name: "v-window : ts" },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        id: 15,
-        name: "Downloads :",
-        children: [
-          { id: 16, name: "October : pdf" },
-          { id: 17, name: "November : pdf" },
-          { id: 18, name: "Tutorial : html" },
-        ],
-      },
-      {
-        id: 19,
-        name: "Videos :",
-        children: [
-          {
-            id: 20,
-            name: "Tutorials :",
-            children: [
-              { id: 21, name: "Basic layouts : mp4" },
-              { id: 22, name: "Advanced techniques : mp4" },
-              { id: 23, name: "All about app : dir" },
-            ],
-          },
-          { id: 24, name: "Intro : mov" },
-          { id: 25, name: "Conference introduction : avi" },
-        ],
-      },
-    ],
-  }),
 };
 </script>
 
